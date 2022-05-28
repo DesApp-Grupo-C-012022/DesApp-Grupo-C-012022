ba5a5685df80e9cb5513f5a14e0a0274024a1441
Juan Cruz Vincenti <juancruzvincenti@gmail.com>
2022-05-28 11:07:28 -0300

    Remove unused import * Fix readme to mark as completed first deliver

--- src/main/kotlin/ar/edu/unq/desapp/grupoC012022/backenddesappapi/models/CurrencyDeserializer.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoC012022/backenddesappapi/models/CurrencyDeserializer.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer
-import com.fasterxml.jackson.databind.node.DoubleNode
 
 
 class CurrencyDeserializer: StdDeserializer<Currency>(Currency::class.java) {
